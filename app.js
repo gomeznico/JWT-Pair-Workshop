1c77c23523f33b05aa3e629c573476144eda19af
Nico Gomez <nicolasgomez1994@gmail.com>
2022-05-23 13:35:33 -0700

    completed workshop

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const app = express();
 app.use(express.json());
 const {
-  models: { User },
+  models: { User, Note },
 } = require('./db');
 const path = require('path');
 const jwt = require('jsonwebtoken');
+const req = require('express/lib/request');
+
+const requireToken = async function (req, res, next) {
+  try {
+    const user = await User.byToken(req.headers.authorization);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
@@ -17,9 +29,9 @@ app.post('/api/auth', async (req, res, next) => {
   }
 });
 
-app.get('/api/auth', async (req, res, next) => {
+app.get('/api/auth', requireToken, async (req, res, next) => {
   try {
-    res.send(await User.byToken(req.headers.authorization));
+    res.send(req.user);
   } catch (ex) {
     next(ex);
   }
@@ -36,6 +48,20 @@ app.delete('/api/auth', async (req, res, next) => {
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status || 500).send({ error: err.message });
+});
+
+app.get('/api/auth/notes', requireToken, async (req, res, next) => {
+  try {
+    const user = req.user;
+    const notes = await Note.findAll({
+      where: {
+        userId: user.id,
+      },
+    });
+    res.send(notes);
+  } catch (ex) {
+    next(ex);
+  }
 });
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const { STRING } = Sequelize;
 const config = {
   logging: false,
@@ -18,44 +19,93 @@ const User = conn.define('user', {
   password: STRING,
 });
 
+const Note = conn.define('note', {
+  text: STRING,
+});
+
+User.hasMany(Note);
+Note.belongsTo(User);
+
 User.byToken = async (token) => {
   try {
     const decoded = jwt.verify(token, 'process.env.JWT');
-    const user = await User.findByPk(decoded.userId);
+    const user = await User.findByPk(decoded.userId );
     return user;
   } catch (ex) {
-    const error = Error('bad credentials');
+    const error = Error('BAD TOKEN bad credentials');
     error.status = 401;
     throw error;
   }
 };
 
 User.authenticate = async ({ username, password }) => {
-  const user = await User.findOne({
-    where: {
-      username,
-      password,
-    },
-  });
-  if (user) {
-    const token = jwt.sign({ userId: user.id }, 'process.env.JWT');
-    return token;
+  try {
+    const user = await User.findOne({
+      where: {
+        username: username,
+      },
+    });
+    if (bcrypt.compareSync(password, user.password)) {
+      const token = await jwt.sign({ userId: user.id }, 'process.env.JWT');
+      return token;
+    }
+    const error = Error('bad credentials');
+    error.status = 403;
+    throw error;
+  } catch (ex) {
+    const error = Error('user not found');
+    error.status = 401;
+    throw error;
   }
-  const error = Error('bad credentials');
-  error.status = 401;
-  throw error;
 };
+
+User.beforeCreate((user, options) => {
+  const hashed = bcrypt.hashSync(user.password, 10);
+  user.password = hashed;
+});
 
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const credentials = [
-    { username: 'lucy', password: 'lucy_pw' },
-    { username: 'moe', password: 'moe_pw' },
-    { username: 'larry', password: 'larry_pw' },
+    {
+      username: 'lucy',
+      password: 'lucy_pw',
+    },
+    {
+      username: 'moe',
+      password: 'moe_pw',
+    },
+    {
+      username: 'larry',
+      password: 'larry_pw',
+    },
   ];
   const [lucy, moe, larry] = await Promise.all(
     credentials.map((credential) => User.create(credential))
   );
+  const newNotes = [
+    {
+      text: '1Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id augue et diam malesuada ultrices in id nunc. In mollis mi et vehicula placerat.',
+    },
+    {
+      text: '2temporary',
+    },
+    {
+      text: '3the death star.',
+    },
+    {
+      text: '4teenage mutant ninja turtles',
+    },
+  ];
+  const [note1, note2, note3, note4] = await Promise.all(
+    newNotes.map((note) => Note.create(note))
+  );
+
+  lucy.addNote(note1);
+  lucy.addNote(note2);
+  moe.addNote(note3);
+  larry.addNote(note4);
+
   return {
     users: {
       lucy,
@@ -69,5 +119,6 @@ module.exports = {
   syncAndSeed,
   models: {
     User,
+    Note,
   },
 };
